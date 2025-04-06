fc4465dc0cfed50aae6072adc1523e338ae17c4f
VitorSLima <dev.vitorlima@gmail.com>
2025-04-05 21:41:01 -0400

    feat: add ProcessTypeModule to application imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,17 @@ import { GoogleStrategy } from './shared/strategies/google.strategy';
 import { AuthService } from './modules/auth/auth.service';
 import { ActivityModule } from './activity/activity.module';
 import { UnitModule } from './modules/unit/unit.module';
+import { ProcessTypeModule } from './modules/process-type/process-type.module';
 
 @Module({
-  imports: [StepModule, UserModule, AuthModule, ActivityModule, UnitModule],
+  imports: [
+    StepModule,
+    UserModule,
+    AuthModule,
+    ActivityModule,
+    UnitModule,
+    ProcessTypeModule,
+  ],
   controllers: [AppController],
   providers: [AppService, AuthService, GoogleStrategy, Logger],
 })
