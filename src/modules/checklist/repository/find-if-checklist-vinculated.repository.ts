d0c72d067d29113c1d89edf5869a122d9cc059f4
farxc <rafaelflamengo07@outlook.com>
2025-04-16 12:18:28 -0400

    feat: created repository to validate checklist

--- src/modules/checklist/repository/find-if-checklist-vinculated.repository.ts
+++ src/modules/checklist/repository/find-if-checklist-vinculated.repository.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../shared/databases/prisma.database';
-
 @Injectable()
 export class FindIfChecklistVinculatedRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async findIfVinculated(checklistId: string) {
     const hasProcess =
-      (await this.prisma.checklist.findFirst({
+      (await this.prisma.checklist.findUnique({
         where: {
           id: checklistId,
           process: {
