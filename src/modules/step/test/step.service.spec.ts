58478ebced42aa1ddf83bd82afb16d6c0d6c0077
Luquet4 <lucasgabrielvmb@gmail.com>
2025-04-29 21:00:16 -0400

    test: add FindAllStepByOrderUseCase provider to StepService tests

--- src/modules/step/test/step.service.spec.ts
+++ src/modules/step/test/step.service.spec.ts
@@ -3,6 +3,7 @@ import { StepService } from '../step.service';
 import {
   CreateStepUseCase,
   DeleteStepUseCase,
+  FindAllStepByOrderUseCase,
   FindAllStepsUseCase,
   FindStepByIdUseCase,
   UpdateStepUseCase,
@@ -42,6 +43,12 @@ describe('StepService', () => {
         },
         {
           provide: DeleteStepUseCase,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: FindAllStepByOrderUseCase,
           useValue: {
             execute: jest.fn(),
           },
